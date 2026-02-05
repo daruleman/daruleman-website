f43a0589f153c0370c7c622d8e37193afad2d1b7
daruleman <daruleman.website@gmail.com>
2026-02-05 18:59:27 +0500

    quiz logic

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/newadmissions',
+        destination: 'https://docs.google.com/forms/d/e/1FAIpQLSdmiWhOGLIY85yFM1y4lQATv5ohvdumzlQGx9vg1wBWbVzTfg/viewform',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
